011eeea4bdb485c36305db1ee0d09dcc6ef5ff7c
Minov Vitaliy <minov.vitaliy@tirolhelpsukraine.eu>
2023-06-13 23:02:05 +0200

    my homework

--- original-index.js
+++ original-index.js
@@ -3,7 +3,6 @@
 function stringToNumber(str) {
 	// Використовуємо Number() для перетворення рядка в число
 	// Повертаємо отримане число
-	return Number(str);
 }
 
 console.log("Завдання 1 ====================================");
@@ -13,11 +12,6 @@ console.log('stringToNumber("42")', stringToNumber("42")); // Виведе 42
 
 function isAlmostSame(num1, num2) {
 	// Визначаємо, чи є різниця між двома числами меншою або рівною EPSILON
-	if (num1 - num2 && num2 - num1 <= Number.EPSILON) {
-		return true;
-	} else {
-		return false;
-	}
 }
 
 console.log("Завдання 2 ====================================");
@@ -27,12 +21,6 @@ console.log("isAlmostSame(0.1 + 0.2, 0.3)", isAlmostSame(0.1 + 0.2, 0.3)); // В
 
 function isSafeInteger(num) {
 	// Перевіряємо, чи є число меншим або рівним MAX_SAFE_INTEGER і більшим або рівним MIN_SAFE_INTEGER
-	if (num <= Number.MAX_SAFE_INTEGER && num >= Number.MIN_SAFE_INTEGER) {
-		return true;
-	} else {
-		return false;
-	}
-
 }
 
 console.log("Завдання 3 ====================================");
@@ -45,11 +33,6 @@ console.log(
 
 function isUnsafeInteger(num) {
 	// Використовуємо логічну оператор НЕ (!), щоб отримати протилежну відповідь від функції isSafeInteger()
-	if (!isSafeInteger(num)) {
-		return true;
-	} else {
-		return false;
-	}
 }
 
 console.log("Завдання 4 ====================================");
@@ -62,11 +45,6 @@ console.log(
 
 function isTooLarge(num) {
 	// Перевіряємо, чи є число більшим за MAX_VALUE
-	if (num > Number.MAX_VALUE) {
-		return true;
-	} else {
-		return false;
-	}
 }
 
 console.log("Завдання 5 ====================================");
@@ -79,11 +57,6 @@ console.log(
 
 function isAlmostZero(num) {
 	// Перевіряємо, чи є число більше 0, але все ще менше за Number.MIN_VALUE
-	if (num > 0 && num < Number.MIN_VALUE) {
-		return false;
-	} else {
-		return true;
-	}
 }
 
 console.log("Завдання 6 ====================================");
@@ -97,11 +70,7 @@ console.log("isAlmostZero(Number.MIN_VALUE)", isAlmostZero(Number.MIN_VALUE)); /
 
 function checkIsInteger(num) {
 	// Використовуємо вбудовану функцію Number.isInteger(), щоб перевірити, чи є значення цілим числом
-	if (Number.isInteger(num)) {
-		return true;
-	} else {
-		return false;
-	}
+
 }
 
 console.log("Завдання 7 ====================================");
@@ -111,11 +80,6 @@ console.log("checkIsInteger(42.5)", checkIsInteger(42.5)); // Виведе false
 
 function checkIsSafeInteger(num) {
 	// Використовуємо вбудовану функцію Number.isSafeInteger(), щоб перевірити, чи є значення безпечним цілим числом
-	if (Number.isSafeInteger(!num)) {
-		return true;
-	} else {
-		return false;
-	}
 }
 
 console.log("Завдання 8 ====================================");
@@ -128,8 +92,6 @@ console.log(
 
 function convertToExponential(num) {
 	// Використовуємо метод toExponential(), щоб конвертувати число в рядок з експоненційним представленням
-	console.log(num.toExponential());
-
 }
 
 console.log("Завдання 9 ====================================");
@@ -139,7 +101,6 @@ console.log("convertToExponential(42)", convertToExponential(42)); // Вивед
 
 function convertToFixed(num, precision) {
 	// Використовуємо метод toFixed(), щоб конвертувати число в рядок з фіксованою кількістю знаків після коми
-	console.log(num.toFixed(2));
 }
 
 console.log("Завдання 10 ====================================");
@@ -149,7 +110,6 @@ console.log("convertToFixed(42.9876, 2)", convertToFixed(42.9876, 2)); // Вив
 
 function convertToString(num) {
 	// Використовуємо метод toString(), щоб конвертувати число в рядок
-	console.log(num.toString());
 }
 
 console.log("Завдання 11 ====================================");
@@ -159,7 +119,6 @@ console.log("convertToString(42)", convertToString(42)); // Виведе "42"
 
 function convertToPrecision(num, precision) {
 	// Використовуємо метод toPrecision(), щоб округлити число до вказаної довжини
-	console.log(num.toPrecision(precision));
 }
 
 console.log("Завдання 12 ====================================");
